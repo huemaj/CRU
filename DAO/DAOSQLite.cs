dccf26555c9b9f76aa7143b1cb21457b784d652f
huemaj <32819717+huemaj@users.noreply.github.com>
2017-10-16 21:59:35 +0200

    baza conn

---
+++ DAO/DAOSQLite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace DAO
+{
+    class DAOSQLite
+    {
+        private SQLiteConnection conn;
+
+        public DAOSQLite()
+        {
+            conn = new SQLiteConnection("Data Source= ./databasepath/cru.db");
+        }
+
+    }
+}
